9bae6b79cb10b46267a31cc246c06f7c6bc1440e
Adetia Marhadi <adetia.ikon@maybank.co.id>
2024-10-18 17:00:01 +0700

    add POST endpoint

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,5 +12,10 @@ public class IndexController {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello";
+    }
+
+    @PostMapping("/hai")
+    public String sayHai() {
+        return "Hai";
     }
 }
